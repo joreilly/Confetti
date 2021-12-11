67cbf8848d8772bb10d8dedc634204e805dae5dc
John O'Reilly <oreillyj@gmail.com>
2021-12-11 11:17:23 +0000

    m1 related changes

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -11,7 +11,7 @@ kotlin {
     listOf(
         iosX64(),
         iosArm64(),
-        //iosSimulatorArm64() sure all ios dependencies support this target
+        iosSimulatorArm64()
     ).forEach {
         it.binaries.framework {
             baseName = "shared"
@@ -43,21 +43,21 @@ kotlin {
         }
         val iosX64Main by getting
         val iosArm64Main by getting
-        //val iosSimulatorArm64Main by getting
+        val iosSimulatorArm64Main by getting
         val iosMain by creating {
             dependsOn(commonMain)
             iosX64Main.dependsOn(this)
             iosArm64Main.dependsOn(this)
-            //iosSimulatorArm64Main.dependsOn(this)
+            iosSimulatorArm64Main.dependsOn(this)
         }
         val iosX64Test by getting
         val iosArm64Test by getting
-        //val iosSimulatorArm64Test by getting
+        val iosSimulatorArm64Test by getting
         val iosTest by creating {
             dependsOn(commonTest)
             iosX64Test.dependsOn(this)
             iosArm64Test.dependsOn(this)
-            //iosSimulatorArm64Test.dependsOn(this)
+            iosSimulatorArm64Test.dependsOn(this)
         }
     }
 }

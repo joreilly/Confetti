ea1372d5f76ffc457fadde0e3439f32879705333
John O'Reilly <oreillyj@gmail.com>
2022-01-25 20:59:11 +0000

    agp plugin 7.1.0 and gradle 7.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.4")
+        classpath("com.android.tools.build:gradle:7.1.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlinVersion}")
         classpath("com.apollographql.apollo3:apollo-gradle-plugin:${Versions.apollo}")
         classpath("com.rickclephas.kmp:kmp-nativecoroutines-gradle-plugin:${Versions.kmpNativeCoroutines}")

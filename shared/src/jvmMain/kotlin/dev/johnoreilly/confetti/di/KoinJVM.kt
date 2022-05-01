1d87fb83a4d4cdebb1691a0a36b89ef9f355e3d8
John O'Reilly <oreillyj@gmail.com>
2022-05-01 16:32:12 +0100

    add JVM platform module settings

--- shared/src/jvmMain/kotlin/dev/johnoreilly/confetti/di/KoinJVM.kt
+++ shared/src/jvmMain/kotlin/dev/johnoreilly/confetti/di/KoinJVM.kt
@@ -1,8 +1,13 @@
 package dev.johnoreilly.confetti.di
 
+import com.apollographql.apollo3.cache.normalized.api.NormalizedCacheFactory
+import com.apollographql.apollo3.cache.normalized.sql.SqlNormalizedCacheFactory
+import com.russhwolf.settings.JvmPreferencesSettings
+import com.russhwolf.settings.ObservableSettings
 import org.koin.dsl.module
+import java.util.prefs.Preferences
 
 actual fun platformModule() = module {
-
-    
+    single<ObservableSettings> { JvmPreferencesSettings(Preferences.userRoot()) }
+    single<NormalizedCacheFactory> { SqlNormalizedCacheFactory("jdbc:sqlite:confetti.db") }
 }

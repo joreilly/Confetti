12a5eeebde72798c414a7a6556718f140762e9ac
Martin Bonnin <martin@mbonnin.net>
2022-10-13 20:07:14 +0200

    fetch everything at startup time

--- shared/src/commonMain/kotlin/dev/johnoreilly/confetti/ConfettiRepository.kt
+++ shared/src/commonMain/kotlin/dev/johnoreilly/confetti/ConfettiRepository.kt
@@ -7,7 +7,9 @@ import com.apollographql.apollo3.cache.normalized.fetchPolicy
 import com.apollographql.apollo3.cache.normalized.isFromCache
 import com.apollographql.apollo3.cache.normalized.watch
 import com.rickclephas.kmp.nativecoroutines.NativeCoroutineScope
+import dev.johnoreilly.confetti.fragment.RoomDetails
 import dev.johnoreilly.confetti.fragment.SessionDetails
+import dev.johnoreilly.confetti.fragment.SpeakerDetails
 import dev.johnoreilly.confetti.utils.DateTimeFormatter
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.MainScope
@@ -33,43 +35,62 @@ class ConfettiRepository : KoinComponent {
     private val appSettings: AppSettings by inject()
     private val dateTimeFormatter: DateTimeFormatter by inject()
 
-    private var timeZone: TimeZone = TimeZone.currentSystemDefault()
-
     val enabledLanguages = appSettings.enabledLanguages
 
-    // TODO: We fetch the first page only, assuming there are <100 conferences. Pagination should be implemented instead.
-    val sessions: Flow<List<SessionDetails>> = apolloClient.query(GetSessionsQuery(first = Optional.Present(100)))
-        .fetchPolicy(FetchPolicy.CacheAndNetwork)
-        .watch().map {
-            it.dataAssertNoErrors.sessions.nodes
-                .map {
-                    it.sessionDetails
-                }
-                .sortedBy { it.startInstant }
+    private sealed interface EverythingResult
+    private class EverythingSuccess(val data: GetEverythingQuery.Data) : EverythingResult
+    private class EverythingError(val throwable: Throwable) : EverythingResult
+    private object EverythingLoading : EverythingResult
+
+    private val everything = MutableStateFlow<EverythingResult>(EverythingLoading)
+
+    val sessions: Flow<List<SessionDetails>>
+        get() {
+            return everything.filterIsInstance<EverythingSuccess>().map {
+                it.data.sessions.nodes.map { it.sessionDetails }.sortedBy { it.startInstant }
+            }
         }
 
     val sessionsMap: Flow<Map<LocalDate, List<SessionDetails>>> = sessions.map {
-        it.groupBy { it.startInstant.toLocalDateTime(timeZone).date }
+        it.groupBy { it.startInstant.toLocalDateTime(TimeZone.of(currentData.config.timezone)).date }
     }
 
-    val speakers = apolloClient.query(GetSpeakersQuery()).watch().map {
-        it.dataAssertNoErrors.speakers.map { it.speakerDetails }
-    }
+    val speakers: Flow<List<SpeakerDetails>>
+        get() {
+            return everything.filterIsInstance<EverythingSuccess>().map { it.data.speakers.map { it.speakerDetails } }
+        }
 
-    val rooms = apolloClient.query(GetRoomsQuery()).watch().map {
-        it.dataAssertNoErrors.rooms.map { it.roomDetails }
-    }
+    val rooms: Flow<List<RoomDetails>>
+        get() {
+            return everything.filterIsInstance<EverythingSuccess>().map { it.data.rooms.map { it.roomDetails } }
+        }
 
     init {
         coroutineScope.launch {
-            val configResponse = apolloClient.query(GetConfigurationQuery()).execute()
-            configResponse.data?.config?.timezone?.let {
-                timeZone = TimeZone.of(it)
-            }
+            // TODO: We fetch the first page only, assuming there are <100 conferences. Pagination should be implemented instead.
+            apolloClient.query(GetEverythingQuery(first = Optional.present(100)))
+                .fetchPolicy(FetchPolicy.CacheAndNetwork)
+                .toFlow()
+                .map { EverythingSuccess(it.dataAssertNoErrors) as EverythingResult }
+                .catch {
+                    emit(EverythingError(it))
+                }.collect {
+                    everything.value = it
+                }
         }
     }
 
+    private val currentData: GetEverythingQuery.Data
+        get() {
+            val everything = everything.value as? EverythingSuccess
+            check(everything != null) {
+                "Cannot call getSessionTime before we fetch the timeZone"
+            }
+            return everything.data
+        }
+
     fun getSessionTime(session: SessionDetails): String {
+        val timeZone = TimeZone.of(currentData.config.timezone)
         return dateTimeFormatter.format(session.startInstant, timeZone, "HH:mm")
     }
 

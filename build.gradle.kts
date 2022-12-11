ea70345fd411095833e84e6eb39b316cb591c389
Martin Bonnin <martin@mbonnin.net>
2022-12-11 20:05:16 +0100

    switch to version catalogs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,28 +1,14 @@
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.0.0-alpha09")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlinVersion}")
-        classpath("com.apollographql.apollo3:apollo-gradle-plugin:${Versions.apollo}")
-        classpath("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:1.8.0-RC-1.0.8")
-        classpath("com.rickclephas.kmp:kmp-nativecoroutines-gradle-plugin:${Versions.kmpNativeCoroutines}")
-        classpath("org.jetbrains.kotlin:kotlin-allopen:${Versions.kotlinVersion}")
-        classpath("org.jetbrains.kotlin:kotlin-serialization:${Versions.kotlinVersion}")
-        classpath("org.springframework.boot:spring-boot-gradle-plugin:2.5.6")
-        classpath("com.google.cloud.tools:appengine-gradle-plugin:2.4.5")
-    }
-}
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-        maven("https://androidx.dev/storage/compose-compiler/repository")
-    }
+plugins {
+    alias(libs.plugins.android.application).apply(false)
+    alias(libs.plugins.kotlin.multiplatform).apply(false)
+    alias(libs.plugins.apollo).apply(false)
+    alias(libs.plugins.ksp).apply(false)
+    alias(libs.plugins.kmp.nativecoroutines).apply(false)
+    alias(libs.plugins.kotlin.serialization).apply(false)
+    alias(libs.plugins.kotlin.spring).apply(false)
+    alias(libs.plugins.spring.boot).apply(false)
+    alias(libs.plugins.appengine).apply(false)
+    alias(libs.plugins.kmmbridge).apply(false)
 }
 
 tasks.register("clean", Delete::class) {

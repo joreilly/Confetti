24a4fa6a3a54e47157e359cb11df065e6718f36e
Yuri Schimke <yuri@schimke.ee>
2022-12-20 23:59:12 +1000

    Update Details Page

--- wearApp/src/main/java/dev/johnoreilly/confetti/wear/sessiondetails/SessionSpeakerInfo.kt
+++ wearApp/src/main/java/dev/johnoreilly/confetti/wear/sessiondetails/SessionSpeakerInfo.kt
@@ -1,6 +1,5 @@
 package dev.johnoreilly.confetti.wear.sessiondetails
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
@@ -11,7 +10,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.wear.compose.material.MaterialTheme
 import androidx.wear.compose.material.Text
@@ -20,7 +18,6 @@ import coil.request.ImageRequest
 import dev.johnoreilly.confetti.R
 import dev.johnoreilly.confetti.fragment.SpeakerDetails
 import dev.johnoreilly.confetti.fullNameAndCompany
-import dev.johnoreilly.confetti.wear.ui.component.SocialIcon
 
 @Composable
 fun SessionSpeakerInfo(
@@ -28,11 +25,11 @@ fun SessionSpeakerInfo(
     speaker: SpeakerDetails,
     onSocialLinkClick: (SpeakerDetails.Social, SpeakerDetails) -> Unit
 ) {
-    Column(modifier.padding(top = 16.dp)) {
+    Column(modifier = modifier) {
         Row {
             AsyncImage(
                 modifier = Modifier
-                    .size(64.dp)
+                    .size(32.dp)
                     .clip(CircleShape),
                 placeholder = painterResource(R.drawable.ic_person_black_24dp),
                 error = painterResource(R.drawable.ic_person_black_24dp),
@@ -46,38 +43,21 @@ fun SessionSpeakerInfo(
             Column(Modifier.padding(horizontal = 8.dp)) {
                 Text(
                     text = speaker.fullNameAndCompany(),
-                    style = MaterialTheme.typography.title2,
-                    fontWeight = FontWeight.Bold
+                    style = MaterialTheme.typography.body1,
                 )
 
-                speaker.city?.let { city ->
-                    Text(
-                        text = city,
-                        style = MaterialTheme.typography.title3
-                    )
-                }
-
-                speaker.bio?.let { bio ->
-                    Text(
-                        modifier = Modifier.padding(top = 12.dp),
-                        text = bio,
-                        style = MaterialTheme.typography.body2
-                    )
-                }
-
-
-                Row(
-                    Modifier.padding(top = 8.dp),
-                    horizontalArrangement = Arrangement.spacedBy(8.dp)
-                ) {
-                    speaker.socials.forEach { socialsItem ->
-                        SocialIcon(
-                            modifier = Modifier.size(24.dp),
-                            socialItem = socialsItem,
-                            onClick = { onSocialLinkClick(socialsItem, speaker) }
-                        )
-                    }
-                }
+//                Row(
+//                    Modifier.padding(top = 8.dp),
+//                    horizontalArrangement = Arrangement.spacedBy(8.dp)
+//                ) {
+//                    speaker.socials.forEach { socialsItem ->
+//                        SocialIcon(
+//                            modifier = Modifier.size(24.dp),
+//                            socialItem = socialsItem,
+//                            onClick = { onSocialLinkClick(socialsItem, speaker) }
+//                        )
+//                    }
+//                }
             }
         }
     }

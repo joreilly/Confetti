1e3653dbdc7e399023d2ddc60170055d3c6417a8
Martin Bonnin <martin@mbonnin.net>
2022-12-30 10:19:29 +0100

    remove workaround for https://issuetracker.google.com/issues/260755411

--- shared/src/androidMain/kotlin/dev/johnoreilly/confetti/utils/AndroidDateTimeFormatter.kt
+++ shared/src/androidMain/kotlin/dev/johnoreilly/confetti/utils/AndroidDateTimeFormatter.kt
@@ -7,9 +7,6 @@ import kotlinx.datetime.toJavaInstant
 import java.time.ZoneId
 
 class AndroidDateTimeFormatter: DateTimeFormatter {
-    // Suppress the error below.
-    // Error: Cast from Instant to TemporalAccessor requires API level 26 (current min is 21) [NewApi]
-    @SuppressLint("NewApi")
     override fun format(instant: Instant, timeZone: TimeZone, format: String): String {
         val formatter = java.time.format.DateTimeFormatter.ofPattern(format)
         return formatter.withZone(ZoneId.of(timeZone.id)).format(instant.toJavaInstant())

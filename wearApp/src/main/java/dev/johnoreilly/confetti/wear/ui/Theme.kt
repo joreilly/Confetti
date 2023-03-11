d2eaad0a3aab0756abc932bfb6728fad560771a8
Yuri Schimke <yuri@schimke.ee>
2023-03-11 11:27:29 +0000

    Adopt Compose future Text padding now

--- wearApp/src/main/java/dev/johnoreilly/confetti/wear/ui/Theme.kt
+++ wearApp/src/main/java/dev/johnoreilly/confetti/wear/ui/Theme.kt
@@ -1,8 +1,10 @@
 package dev.johnoreilly.confetti.wear.ui
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.graphics.Color.Companion.Black
 import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.text.PlatformTextStyle
 import androidx.wear.compose.material.Colors
 import androidx.wear.compose.material.MaterialTheme
 
@@ -29,9 +31,27 @@ val ColorScheme = Colors(
  */
 @Composable
 fun ConfettiTheme(
-    content: @Composable() () -> Unit
+    content: @Composable () -> Unit
 ) {
-    MaterialTheme(colors = ColorScheme, content = content)
+    val defaultTypography = MaterialTheme.typography
+    @Suppress("DEPRECATION")
+    val typography = remember {
+        defaultTypography.copy(
+            display1 = defaultTypography.display1.copy(platformStyle = PlatformTextStyle(false)),
+            display2 = defaultTypography.display2.copy(platformStyle = PlatformTextStyle(false)),
+            display3 = defaultTypography.display3.copy(platformStyle = PlatformTextStyle(false)),
+            title1 = defaultTypography.title1.copy(platformStyle = PlatformTextStyle(false)),
+            title2 = defaultTypography.title2.copy(platformStyle = PlatformTextStyle(false)),
+            title3 = defaultTypography.title3.copy(platformStyle = PlatformTextStyle(false)),
+            body1 = defaultTypography.body1.copy(platformStyle = PlatformTextStyle(false)),
+            body2 = defaultTypography.body2.copy(platformStyle = PlatformTextStyle(false)),
+            button = defaultTypography.button.copy(platformStyle = PlatformTextStyle(false)),
+            caption1 = defaultTypography.caption1.copy(platformStyle = PlatformTextStyle(false)),
+            caption2 = defaultTypography.caption2.copy(platformStyle = PlatformTextStyle(false)),
+            caption3 = defaultTypography.caption3.copy(platformStyle = PlatformTextStyle(false)),
+        )
+    }
+    MaterialTheme(colors = ColorScheme, content = content, typography = typography)
 }
 
 fun Colors.toTileColors(): androidx.wear.tiles.material.Colors =

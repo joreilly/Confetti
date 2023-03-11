2f03111f69a8de56edc8697aa31fa45b01aea7e2
Yuri Schimke <yuri@schimke.ee>
2023-03-11 16:26:56 +0000

    Introduce UI State models.

--- wearApp/src/main/java/dev/johnoreilly/confetti/wear/home/homeView.kt
+++ wearApp/src/main/java/dev/johnoreilly/confetti/wear/home/homeView.kt
@@ -2,6 +2,7 @@
 
 package dev.johnoreilly.confetti.wear.home
 
+import androidx.activity.compose.ReportDrawnWhen
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -9,6 +10,7 @@ import com.google.android.horologist.compose.layout.ScalingLazyColumnState
 import com.google.android.horologist.compose.navscaffold.ExperimentalHorologistComposeLayoutApi
 import dev.johnoreilly.confetti.navigation.ConferenceDayKey
 import dev.johnoreilly.confetti.navigation.SessionDetailsKey
+import dev.johnoreilly.confetti.wear.conferences.ConferencesUiState
 import org.koin.androidx.compose.getViewModel
 
 @Composable
@@ -21,6 +23,10 @@ fun HomeRoute(
     viewModel: HomeViewModel = getViewModel()
 ) {
     val uiState = viewModel.uiState.collectAsStateWithLifecycle().value
+
+    ReportDrawnWhen {
+        uiState !is HomeUiState.Loading
+    }
 
     SideEffect {
         if (uiState is HomeUiState.NoneSelected) {

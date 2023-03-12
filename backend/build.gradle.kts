e6468b90d3bfa730ff9472baec78a5e888830314
Martin Bonnin <martin@mbonnin.net>
2023-03-12 15:13:16 +0100

    add a landing page

--- backend/build.gradle.kts
+++
@@ -1,5 +0,0 @@
-tasks.register("setupGoogleServices") {
-  doLast {
-    file("service_account_key.json").writeText(System.getenv("GOOGLE_SERVICES_JSON"))
-  }
-}
---
+++ buildSrc/src/main/kotlin/landing.kt
@@ -0,0 +1,41 @@
+import com.google.auth.oauth2.GoogleCredentials
+import com.google.cloud.storage.BlobId
+import com.google.cloud.storage.BlobInfo
+import com.google.cloud.storage.Bucket
+import com.google.cloud.storage.Storage
+import com.google.cloud.storage.StorageOptions
+import org.gradle.api.Project
+import java.io.File
+
+
+fun Project.uploadLandingPage() {
+    val storage: Storage = StorageOptions.newBuilder()
+        .setCredentials(file("../backend/gcp_service_account_key.json").inputStream().use {
+            GoogleCredentials.fromStream(it)
+        })
+        .build()
+        .service
+
+    val bucketName = "confetti-landing-page"
+
+
+    println("uploading landing page...")
+    val base = file("public")
+    base.walk().filter { it.isFile }
+        .forEach { file ->
+            file.inputStream().use {
+                val blobId = BlobId.of(bucketName, file.relativeTo(base).path)
+                val blobInfo = BlobInfo.newBuilder(blobId).setContentType(file.contentType()).build()
+
+                storage.createFrom(blobInfo, it)
+            }
+        }
+}
+
+private fun File.contentType() : String{
+    return when (this.extension) {
+        "html", "htm" -> "text/html"
+        "css" -> "text/css"
+        else -> "application/octet-stream"
+    }
+}

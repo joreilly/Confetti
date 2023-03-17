9552bdac985ec7ad2f18da9a36b0391d6242db94
John O'Reilly <oreillyj@gmail.com>
2023-03-17 21:25:19 +0000

    add swift unit test + run as part of CI

--- iosApp/ConfettiTests/ConfettiTests.swift
+++ iosApp/ConfettiTests/ConfettiTests.swift
@@ -15,10 +15,21 @@ final class ConfettiTests: XCTestCase {
     func testGetConferences() async throws {
         let viewModel = ConferencesViewModel()
         
-        let confUIStateSequence = asyncSequence(for: viewModel.uiStateFlow)
-        let uiState = try await confUIStateSequence.first(where: { $0 is ConferencesViewModel.Success })
+        let conferencesUIStateSequence = asyncSequence(for: viewModel.uiStateFlow)
+        let uiState = try await conferencesUIStateSequence.first(where: { $0 is ConferencesViewModel.Success })
         let conferences = (uiState as! ConferencesViewModel.Success).conferences
         XCTAssert(!conferences.isEmpty)
+    }
+
+    
+    func testGetSessions() async throws {
+        let viewModel = SessionsViewModel()
+        viewModel.setConference(conference: "test")
+        
+        let sessionsUIStateSequence = asyncSequence(for: viewModel.uiStateFlow)
+        let uiState = try await sessionsUIStateSequence.first(where: { $0 is SessionsUiStateSuccess })
+        let sessions = (uiState as! SessionsUiStateSuccess).sessionsByStartTimeList
+        XCTAssert(!sessions.isEmpty)
     }
 
 }

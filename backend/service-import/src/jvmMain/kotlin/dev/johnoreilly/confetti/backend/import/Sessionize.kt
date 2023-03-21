ffbd1fb9d3de3bfde16b3ab0b535163293f016a0
Martin Bonnin <martin@mbonnin.net>
2023-03-21 20:32:51 +0100

    add fake start/end times for Android Makers 2023

--- backend/service-import/src/jvmMain/kotlin/dev/johnoreilly/confetti/backend/import/Sessionize.kt
+++ backend/service-import/src/jvmMain/kotlin/dev/johnoreilly/confetti/backend/import/Sessionize.kt
@@ -1,28 +1,104 @@
 package dev.johnoreilly.confetti.backend.import
 
 import dev.johnoreilly.confetti.backend.datastore.*
+import kotlinx.datetime.LocalDate
 import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.atTime
+import kotlinx.datetime.toInstant
+import kotlinx.datetime.toLocalDateTime
 import net.mbonnin.bare.graphql.*
+import kotlin.time.Duration.Companion.hours
 
 object Sessionize {
     private val droidConLondon2022 = "https://sessionize.com/api/v2/qi0g29hw/view/All"
     private val kotlinConf2023 = "https://sessionize.com/api/v2/rje6khfn/view/All"
     private val androidMakers2023 = "https://sessionize.com/api/v2/72i2tw4v/view/All"
 
+
     suspend fun importDroidConLondon2022(): Int {
-        return import(ConferenceId.DroidConLondon2022.id, "droidcon London", droidConLondon2022)
+        return import(
+            droidConLondon2022,
+            config = DConfig(
+                id = ConferenceId.DroidConLondon2022.id,
+                name = "droidcon London",
+                timeZone = "Europe/London"
+            ),
+            venue = DVenue(
+                id = "main",
+                name = "Business Design Center",
+                address = "52 Upper St, London N1 0QH, United Kingdom",
+                description = mapOf(
+                    "en" to "Cool venue",
+                    "fr" to "Venue fraiche",
+                ),
+                latitude = 51.5342463,
+                longitude = -0.1068864,
+                imageUrl = "https://london.droidcon.com/wp-content/uploads/sites/3/2022/07/Venue2-1.png",
+                floorPlanUrl = null
+            )
+        )
     }
 
     suspend fun importKotlinConf2023(): Int {
-        return import(ConferenceId.KotlinConf2023.id, "KotlinConf 2023", kotlinConf2023)
+        return import(
+            kotlinConf2023,
+            config = DConfig(
+                id = ConferenceId.KotlinConf2023.id,
+                name = "KotlinConf 2023",
+                timeZone = "Europe/Amsterdam"
+            ),
+            venue = DVenue(
+                id = "main",
+                name = "Business Design Center",
+                address = "52 Upper St, London N1 0QH, United Kingdom",
+                description = mapOf(
+                    "en" to "Cool venue",
+                    "fr" to "Venue fraiche",
+                ),
+                latitude = 51.5342463,
+                longitude = -0.1068864,
+                imageUrl = "https://london.droidcon.com/wp-content/uploads/sites/3/2022/07/Venue2-1.png",
+                floorPlanUrl = null
+            )
+        )
     }
 
     suspend fun importAndroidMakers2023(): Int {
-        return import(ConferenceId.AndroidMakers2023.id, "AndroidMakers 2023", androidMakers2023)
+        return import(
+            androidMakers2023,
+            config = DConfig(
+                id = ConferenceId.AndroidMakers2023.id,
+                name = "Android Makers 2023",
+                timeZone = "Europe/Paris",
+                days = listOf(LocalDate(2023, 4, 27), LocalDate(2023, 4, 28))
+            ),
+            venue = DVenue(
+                id = "main",
+                name = "Beffroi de Montrouge",
+                address = "Av. de la République, 92120 Montrouge",
+                description = mapOf(
+                    "en" to "Cool venue",
+                    "fr" to "Venue fraiche",
+                ),
+                latitude = null,
+                longitude = null,
+                imageUrl = null,
+                floorPlanUrl = null
+            ),
+            partners = listOf(
+                DPartner(name = "Roquefort", logoUrl = "https://www.fromages.com/media/uploads/fromage/liste_27_1.png", url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"),
+                DPartner(name = "Comté", logoUrl = "https://www.fromages.com/media/uploads/fromage/liste_808_1.png", url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"),
+                DPartner(name = "Camembert", logoUrl = "https://www.fromages.com/media/uploads/fromage/liste_75_1.png", url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"),
+            )
+        )
     }
 
-    private suspend fun import(conf: String, confName: String, url: String): Int {
+    private suspend fun import(url: String, config: DConfig, venue: DVenue, partners: List<DPartner> = emptyList()): Int {
         val data = getJsonUrl(url)
+        val day1 = LocalDate(2023, 4, 27)
+        val day2 = LocalDate(2023, 4, 28)
+        var time = day1.atTime(9, 30)
 
         val categories = data.asMap["categories"].asList.map { it.asMap }
             .flatMap {
@@ -35,20 +111,20 @@ object Sessionize {
         val sessions = data.asMap["sessions"].asList.map {
             it.asMap
         }.mapNotNull {
-            if (it.get("startsAt") == null || it.get("endsAt") == null){
-                /**
-                 * Guard against sessions that are not scheduled.
-                 */
-                return@mapNotNull null
+            var start = time
+            if (start > LocalDateTime(2023, 4, 27, 18, 0)) {
+                start = day2.atTime(9,30)
             }
+            var end  = start.toInstant(TimeZone.UTC).plus(1.hours).toLocalDateTime(TimeZone.UTC)
+
             DSession(
                 id = it.get("id").asString,
                 type = if (it.get("isServiceSession").cast()) "service" else "talk",
                 title = it.get("title").asString,
                 description = it.get("description")?.asString,
                 language = "en-US",
-                start = it.get("startsAt").asString.let { LocalDateTime.parse(it) },
-                end = it.get("endsAt").asString.let { LocalDateTime.parse(it) },
+                start = start,
+                end = end,
                 complexity = null,
                 feedbackId = null,
                 tags = it.get("categoryItems").asList.mapNotNull { categoryId ->
@@ -57,7 +133,9 @@ object Sessionize {
                 rooms = listOf(it.get("roomId").toString()),
                 speakers = it.get("speakers").asList.map { it.asString },
                 shortDescription = null,
-            )
+            ).also {
+                time = end
+            }
         }
 
         val rooms = data.asMap["rooms"].asList.map { it.asMap }.map {
@@ -87,27 +165,9 @@ object Sessionize {
             sessions = sessions.sortedBy { it.start },
             rooms = rooms,
             speakers = speakers,
-            partnerGroups = emptyList(),
-            config = DConfig(
-                id = conf,
-                name = confName,
-                timeZone = "Europe/London"
-            ),
-            venues = listOf(
-                DVenue(
-                    id = "main",
-                    name = "Business Design Center",
-                    address = "52 Upper St, London N1 0QH, United Kingdom",
-                    description = mapOf(
-                        "en" to "Cool venue",
-                        "fr" to "Venue fraiche",
-                    ),
-                    latitude = 51.5342463,
-                    longitude = -0.1068864,
-                    imageUrl = "https://london.droidcon.com/wp-content/uploads/sites/3/2022/07/Venue2-1.png",
-                    floorPlanUrl = null
-                )
-            )
+            partnerGroups = listOf(DPartnerGroup("1", partners)),
+            config = config,
+            venues = listOf(venue)
         )
     }
 }

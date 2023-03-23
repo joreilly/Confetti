8cdb7a4f4ad021e5e41991e073fcfa097620114b
Marcello Galhardo <marcello.galhardo@gmail.com>
2023-03-23 22:49:51 +0000

    Add `share` button

--- androidApp/src/main/java/dev/johnoreilly/confetti/sessiondetails/SessionDetailsView.kt
+++ androidApp/src/main/java/dev/johnoreilly/confetti/sessiondetails/SessionDetailsView.kt
@@ -4,15 +4,32 @@ package dev.johnoreilly.confetti.sessiondetails
 
 import android.content.Intent
 import android.net.Uri
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material3.*
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
@@ -20,7 +37,10 @@ import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import dev.johnoreilly.confetti.SessionDetailsViewModel
 import dev.johnoreilly.confetti.fragment.SessionDetails
+import dev.johnoreilly.confetti.utils.format
 import org.koin.androidx.compose.getViewModel
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
 
 @Composable
 fun SessionDetailsRoute(conference: String, sessionId: String, onBackClick: () -> Unit) {
@@ -28,12 +48,12 @@ fun SessionDetailsRoute(conference: String, sessionId: String, onBackClick: () -
         configure(conference, sessionId)
     }
     val session by viewModel.session.collectAsStateWithLifecycle()
-    SessionDetailView(session, onBackClick)
+    val share = rememberShareDetails(session)
+    SessionDetailView(session, onBackClick, share)
 }
 
-
 @Composable
-fun SessionDetailView(session: SessionDetails?, popBack: () -> Unit) {
+fun SessionDetailView(session: SessionDetails?, popBack: () -> Unit, share: () -> Unit) {
     val scrollState = rememberScrollState()
     val context = LocalContext.current
 
@@ -48,25 +68,35 @@ fun SessionDetailView(session: SessionDetails?, popBack: () -> Unit) {
                 },
                 colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
                     containerColor = Color.Transparent
-                )
+                ),
+                actions = {
+                    IconButton(onClick = { share() }) {
+                        Icon(Icons.Filled.Share, contentDescription = "Share")
+                    }
+                }
             )
         }
     ) {
         Column(modifier = Modifier.padding(it)) {
             session?.let { session ->
                 Column(
-                    modifier = Modifier.fillMaxWidth()
+                    modifier = Modifier
+                        .fillMaxWidth()
                         .padding(horizontal = 16.dp, vertical = 8.dp)
                         .verticalScroll(state = scrollState)
                 ) {
 
-                    Text(text = session.title,
+                    Text(
+                        text = session.title,
                         color = MaterialTheme.colorScheme.primary,
-                        style = MaterialTheme.typography.titleLarge)
+                        style = MaterialTheme.typography.titleLarge
+                    )
 
                     Spacer(modifier = Modifier.size(16.dp))
-                    Text(text = session.sessionDescription ?: "",
-                        style = MaterialTheme.typography.bodyMedium)
+                    Text(
+                        text = session.sessionDescription ?: "",
+                        style = MaterialTheme.typography.bodyMedium
+                    )
 
                     if (session.tags.isNotEmpty()) {
                         Spacer(modifier = Modifier.size(16.dp))
@@ -109,5 +139,49 @@ fun Chip(name: String) {
             color = MaterialTheme.colorScheme.onSecondaryContainer,
             modifier = Modifier.padding(10.dp)
         )
+    }
+}
+
+@Composable
+private fun rememberShareDetails(details: SessionDetails?): () -> Unit {
+    val context = LocalContext.current
+
+    return remember(details) {
+        // If details is null, early return.
+        if (details == null) return@remember {}
+
+        val dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yy")
+        val timeFormatter = DateTimeFormatter.ofPattern("hh:mm")
+
+        val sendIntent = Intent().apply {
+            action = Intent.ACTION_SEND
+            type = "text/plain"
+
+            val room = details.room?.name ?: "Unknown"
+            val date = dateFormatter.format(details.startsAt)
+            val startsAt = timeFormatter.format(details.startsAt)
+            val endsAt = timeFormatter.format(details.endsAt)
+            val speakers = details
+                .speakers
+                .map { it.speakerDetails.name }
+                .toString()
+                .removeSurrounding(prefix = "[", suffix = "]")
+
+            val text =
+                """
+                |Title: ${details.title}
+                |Room: $room
+                |Date: $date
+                |Starts at: $startsAt
+                |Ends at: $endsAt
+                |Speaker: $speakers
+                |---
+                |Description: ${details.sessionDescription}
+                """.trimMargin()
+            putExtra(Intent.EXTRA_TEXT, text)
+        }
+
+        val launchIntent = Intent.createChooser(sendIntent, null)
+        return@remember { context.startActivity(launchIntent) }
     }
 }

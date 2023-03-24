d3f2cbb39797a442c2f3c57bf35df0bf74c68ac3
Marcello Galhardo <marcello.galhardo@gmail.com>
2023-03-24 23:56:09 +0000

    Improve search appearance

--- androidApp/src/main/java/dev/johnoreilly/confetti/ui/ConfettiScaffold.kt
+++ androidApp/src/main/java/dev/johnoreilly/confetti/ui/ConfettiScaffold.kt
@@ -1,6 +1,13 @@
+@file:OptIn(
+    ExperimentalAnimationApi::class, ExperimentalMaterial3Api::class,
+    ExperimentalComposeUiApi::class
+)
+
 package dev.johnoreilly.confetti.ui
 
 import android.annotation.SuppressLint
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -9,6 +16,8 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Search
@@ -18,21 +27,33 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.ShapeDefaults
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.key.Key
+import androidx.compose.ui.input.key.KeyEventType.Companion.KeyUp
+import androidx.compose.ui.input.key.key
+import androidx.compose.ui.input.key.onPreviewKeyEvent
+import androidx.compose.ui.input.key.type
 import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.sp
 import dev.johnoreilly.confetti.account.AccountIcon
 import dev.johnoreilly.confetti.settings.SettingsDialog
@@ -79,63 +100,57 @@ fun ConfettiScaffold(
             // That gives more space for the user to see the screen content.
             scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
         }
+        var isSearching by remember { mutableStateOf(false) }
         Scaffold(
             modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
             snackbarHost = {
                 SnackbarHost(hostState = snackbarHostState)
             },
             topBar = {
-                CenterAlignedTopAppBar(
-                    title = {
-                        if (title != null) {
-                            Text(text = title, fontSize = titleFontSize)
-                        }
-                    },
-                    colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
-                        containerColor = Color.Transparent
-                    ),
-                    navigationIcon = {
-                        AccountIcon(
-                            onSwitchConference = onSwitchConference,
-                            onSignIn = onSignIn,
-                            onSignOut = onSignOut,
-                            onShowSettings = { appState.setShowSettingsDialog(true) },
-                        )
-                    },
-                    actions = {
-                        if (search != null) {
-                            var searchCollapsed by remember { mutableStateOf(true) }
-                            if (searchCollapsed) {
-                                IconButton(onClick = { searchCollapsed = false }) {
-                                    Icon(Icons.Filled.Search, contentDescription = "Search")
-                                }
-                            } else {
-                                TextField(
-                                    modifier = Modifier
-                                        .fillMaxWidth()
-                                        .padding(5.dp),
-                                    value = search,
+                // Animates the transition when starting a search.
+                AnimatedContent(targetState = isSearching, label = "search") { shouldShowSearch ->
+                    if (shouldShowSearch) {
+                        // Wraps search in a top-bar to maintain same appearance and sizing.
+                        CenterAlignedTopAppBar(
+                            title = {
+                                SearchTextField(
+                                    value = search.orEmpty(),
                                     onValueChange = onSearch,
-                                    label = { Text("Search") },
-                                    maxLines = 1,
-                                    singleLine = true,
-                                    trailingIcon = {
-                                        IconButton(onClick = {
-                                            searchCollapsed = true
-                                            onSearch("")
-                                        }) {
-                                            Icon(
-                                                Icons.Filled.Close,
-                                                contentDescription = "Close Search"
-                                            )
-                                        }
+                                    onCloseSearch = {
+                                        isSearching = false
+                                        onSearch("")
                                     }
                                 )
                             }
-                        }
-                    },
-                    scrollBehavior = scrollBehavior,
-                )
+                        )
+                    } else {
+                        CenterAlignedTopAppBar(
+                            title = {
+                                if (title != null) {
+                                    Text(text = title, fontSize = titleFontSize)
+                                }
+                            },
+                            colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                                containerColor = Color.Transparent
+                            ),
+                            navigationIcon = {
+                                AccountIcon(
+                                    onSwitchConference = onSwitchConference,
+                                    onSignIn = onSignIn,
+                                    onSignOut = onSignOut,
+                                    onShowSettings = { appState.setShowSettingsDialog(true) },
+                                )
+                            },
+                            actions = {
+                                IconButton(onClick = { isSearching = true }) {
+                                    Icon(Icons.Filled.Search, contentDescription = "Search")
+                                }
+                            },
+                            scrollBehavior = scrollBehavior,
+                        )
+                    }
+                }
+
             },
             containerColor = Color.Transparent,
             contentWindowInsets = WindowInsets(0, 0, 0, 0)
@@ -159,3 +174,65 @@ fun ConfettiScaffold(
         }
     }
 }
+
+@Composable
+private fun SearchTextField(
+    modifier: Modifier = Modifier,
+    value: String = "",
+    onValueChange: (String) -> Unit,
+    onCloseSearch: () -> Unit,
+) {
+    val keyboardController = LocalSoftwareKeyboardController.current
+    val focusRequester = remember { FocusRequester() }
+
+    SideEffect {
+        focusRequester.requestFocus()
+    }
+
+    TextField(
+        modifier = modifier
+            .focusRequester(focusRequester)
+            .interceptKey(Key.Enter) {
+                keyboardController?.hide()
+            }
+            .fillMaxWidth(),
+        value = value,
+        onValueChange = onValueChange,
+        placeholder = { Text("Search") },
+        leadingIcon = { Icon(Icons.Filled.Search, "Search") },
+        trailingIcon = {
+            IconButton(onClick = { onCloseSearch() }) {
+                Icon(
+                    Icons.Filled.Close,
+                    contentDescription = "Close Search"
+                )
+            }
+        },
+        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
+        keyboardActions = KeyboardActions(
+            onSearch = { keyboardController?.hide() }
+        ),
+        colors = TextFieldDefaults.textFieldColors(
+            focusedIndicatorColor = Color.Transparent, //hide the indicator
+        ),
+        textStyle = ConfettiTypography.bodyLarge,
+        shape = ShapeDefaults.Large,
+        singleLine = true,
+    )
+}
+
+/**
+ * [Modifier] to intercept [key] events and fires [onKeyEvent] callback when the key is released.
+ *
+ * The [key] parameter represents the key to be intercepted
+ * The [onKeyEvent] listener is an optional listener to when the key event happens.
+ *
+ * The intercepted key is not passed to any child composable.
+ */
+fun Modifier.interceptKey(key: Key, onKeyEvent: () -> Unit = {}): Modifier =
+    onPreviewKeyEvent { event ->
+        if (event.key == key && event.type == KeyUp) {
+            onKeyEvent()
+        }
+        event.key == key
+    }

0f9700f2f9efca0ce0ecf10a3544f6313e851ba9
Yuri Schimke <yuri@schimke.ee>
2023-03-25 17:29:18 +0000

    Simplify

--- wearApp/src/main/java/dev/johnoreilly/confetti/wear/auth/FirebaseSignOutViewModel.kt
+++ wearApp/src/main/java/dev/johnoreilly/confetti/wear/auth/FirebaseSignOutViewModel.kt
@@ -3,34 +3,24 @@
 package dev.johnoreilly.confetti.wear.auth
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.google.android.gms.common.api.ApiException
 import com.google.android.horologist.annotations.ExperimentalHorologistApi
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.launch
 
-class FirebaseSignOutViewModel : ViewModel() {
+class FirebaseSignOutViewModel() : ViewModel() {
 
     private val _uiState = MutableStateFlow(GoogleSignOutScreenState.Idle)
     val uiState: StateFlow<GoogleSignOutScreenState> = _uiState
 
     fun onIdleStateObserved() {
-        if (_uiState.compareAndSet(
-                expect = GoogleSignOutScreenState.Idle,
-                update = GoogleSignOutScreenState.Loading
-            )
-        ) {
-            viewModelScope.launch {
-                try {
-                    Firebase.auth.signOut()
-                    _uiState.value = GoogleSignOutScreenState.Success
-                } catch (apiException: ApiException) {
-                    _uiState.value = GoogleSignOutScreenState.Failed
-                }
-            }
+        try {
+            Firebase.auth.signOut()
+            _uiState.value = GoogleSignOutScreenState.Success
+        } catch (apiException: ApiException) {
+            _uiState.value = GoogleSignOutScreenState.Failed
         }
     }
 }

dc4d6842d1becbf8b66c39ac5b36b7a753dd45fa
Marcello Galhardo <marcello.galhardo@gmail.com>
2023-03-25 13:43:56 +0000

    Initialize Logging

--- androidApp/src/main/java/dev/johnoreilly/confetti/ConfettiApplication.kt
+++ androidApp/src/main/java/dev/johnoreilly/confetti/ConfettiApplication.kt
@@ -1,18 +1,16 @@
 package dev.johnoreilly.confetti
 
 import android.app.Application
-import androidx.work.ExistingPeriodicWorkPolicy
-import androidx.work.WorkManager
 import coil.ImageLoader
 import coil.ImageLoaderFactory
-import com.google.firebase.FirebaseApp
 import com.google.firebase.crashlytics.ktx.crashlytics
 import com.google.firebase.crashlytics.ktx.setCustomKeys
 import com.google.firebase.ktx.Firebase
-import dev.johnoreilly.confetti.di.initKoin
 import dev.johnoreilly.confetti.di.appModule
-import dev.johnoreilly.confetti.work.RefreshWorker
+import dev.johnoreilly.confetti.di.initKoin
 import dev.johnoreilly.confetti.work.setupDailyRefresh
+import io.github.aakira.napier.DebugAntilog
+import io.github.aakira.napier.Napier
 import org.koin.android.ext.android.get
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
@@ -32,7 +30,10 @@ class ConfettiApplication : Application(), ImageLoaderFactory {
         } else {
             Firebase.crashlytics.setCrashlyticsCollectionEnabled(false)
         }
-        
+
+        // Initialize Logging.
+        Napier.base(DebugAntilog())
+
         initKoin {
             androidLogger()
             androidContext(this@ConfettiApplication)

d7a0722b013c50210072c83ce5845cdb2c15fb9b
Odin Asbjørnsen <odinas004@gmail.com>
2023-04-02 21:21:58 +0200

    Cleanup wearUIState

--- androidApp/src/main/java/dev/johnoreilly/confetti/account/WearUiState.kt
+++ androidApp/src/main/java/dev/johnoreilly/confetti/account/WearUiState.kt
@@ -5,8 +5,6 @@ import com.google.android.horologist.data.apphelper.AppHelperNodeStatus
 data class WearUiState(
     val wearNodes: List<AppHelperNodeStatus> = listOf()
 ) {
-    val isInstalledOnWear: Boolean
-        get() = wearNodes.find { it.isAppInstalled } != null
     val showInstallOnWear: Boolean
         get() = wearNodes.isNotEmpty() && wearNodes.find { !it.isAppInstalled } != null
 }

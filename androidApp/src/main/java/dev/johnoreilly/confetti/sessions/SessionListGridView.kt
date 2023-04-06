a52dfce1b97f7719857d331b2c8685e138b2d838
stylianosgakis <stylianos.gakis98@gmail.com>
2023-04-07 01:38:58 +0300

    Fix session screen having some unnecessary bottom padding
    Make use of window insets instead, to specifically only pad that much for cases like tablets, while still making sure that in phone form factors the insets are not applied two times.

--- androidApp/src/main/java/dev/johnoreilly/confetti/sessions/SessionListGridView.kt
+++ androidApp/src/main/java/dev/johnoreilly/confetti/sessions/SessionListGridView.kt
@@ -13,12 +13,17 @@ import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowColumn
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
@@ -87,7 +92,9 @@ fun SessionListGridView(
 
                     Row(
                         Modifier
-                            .padding(bottom = 60.dp)
+                            .windowInsetsPadding(
+                                WindowInsets.safeDrawing.only(WindowInsetsSides.Bottom)
+                            )
                             .horizontalScroll(rememberScrollState())
                     ) {
                         val sessionsByStartTime = uiState.sessionsByStartTimeList[page]

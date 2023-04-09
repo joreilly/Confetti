defe8396ac4b911ef791db7ccab34a8a2e537ca7
Marcello Galhardo <marcello.galhardo@gmail.com>
2023-04-09 15:21:22 +0100

    Use `KoinComponent` into `SessionNotificationWorker`

--- shared/src/androidMain/kotlin/dev/johnoreilly/confetti/work/SessionNotificationWorker.kt
+++ shared/src/androidMain/kotlin/dev/johnoreilly/confetti/work/SessionNotificationWorker.kt
@@ -5,20 +5,23 @@ import androidx.work.Constraints
 import androidx.work.CoroutineWorker
 import androidx.work.ExistingPeriodicWorkPolicy
 import androidx.work.ExistingWorkPolicy
-import androidx.work.OneTimeWorkRequest
 import androidx.work.OneTimeWorkRequestBuilder
-import androidx.work.PeriodicWorkRequest
 import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkManager
 import androidx.work.WorkerParameters
 import dev.johnoreilly.confetti.work.SessionNotificationSender.Companion.INTERVAL
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
 import kotlin.time.toJavaDuration
 
+// Using a custom constructor has caused a crash inside Koin.
+// For now, as a workaround, we use KoinComponent with an inject for getting the notifier instance.
 class SessionNotificationWorker(
     context: Context,
     workerParams: WorkerParameters,
-    private val notifier: SessionNotificationSender,
-) : CoroutineWorker(context, workerParams) {
+) : CoroutineWorker(context, workerParams), KoinComponent {
+
+    private val notifier: SessionNotificationSender by inject()
 
     override suspend fun doWork(): Result {
         if (runAttemptCount > 3) {

8b5dd529029dc1d13ebf529aab23757c7b15e63c
John O'Reilly <oreillyj@gmail.com>
2023-06-12 17:01:21 +0100

    KMM SPM package release for 0.8.6

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 import PackageDescription
 
 // BEGIN KMMBRIDGE VARIABLES BLOCK (do not edit)
-let remoteKotlinUrl = "https://repo.repsy.io/mvn/joreilly/confetti/Confetti/shared-kmmbridge/0.8.5/shared-kmmbridge-0.8.5.zip"
+let remoteKotlinUrl = "https://repo.repsy.io/mvn/joreilly/confetti/Confetti/shared-kmmbridge/0.8.6/shared-kmmbridge-0.8.6.zip"
 let remoteKotlinChecksum = "8bab10063985f52928de5d64d2b72b20af28570d2a445bad040740397aee14eb"
 let packageName = "ConfettiKit"
 // END KMMBRIDGE BLOCK

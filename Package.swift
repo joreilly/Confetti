c5dfa791ce574acba9d946b07fc435afd36945ec
runner <runner@Mac-1687012000187.local>
2023-06-17 15:31:47 +0000

    KMM SPM package release for 0.8.13

--- Package.swift
+++ Package.swift
@@ -2,8 +2,8 @@
 import PackageDescription
 
 // BEGIN KMMBRIDGE VARIABLES BLOCK (do not edit)
-let remoteKotlinUrl = "https://repo.repsy.io/mvn/joreilly/confetti/Confetti/shared-kmmbridge/0.8.12/shared-kmmbridge-0.8.12.zip"
-let remoteKotlinChecksum = "70bde00884fc682fceb7a8c63c2f4ea5cb90a8f1d348143ea5f300cecb2b496b"
+let remoteKotlinUrl = "https://repo.repsy.io/mvn/joreilly/confetti/Confetti/shared-kmmbridge/0.8.13/shared-kmmbridge-0.8.13.zip"
+let remoteKotlinChecksum = "381b6f2e7a63ac1ea84de35f6ad55f21b6fe7ccfd6f54e1ca56497aa6abcb3e3"
 let packageName = "ConfettiKit"
 // END KMMBRIDGE BLOCK
 

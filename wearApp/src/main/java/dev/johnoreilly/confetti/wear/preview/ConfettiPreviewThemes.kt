6b56ad3668f241422f624b7632b4f89ba9246dec
Yuri Schimke <yuri@schimke.ee>
2023-09-16 10:29:15 +0100

    Fix build

--- wearApp/src/main/java/dev/johnoreilly/confetti/wear/preview/ConfettiPreviewThemes.kt
+++ wearApp/src/main/java/dev/johnoreilly/confetti/wear/preview/ConfettiPreviewThemes.kt
@@ -1,15 +1,33 @@
 package dev.johnoreilly.confetti.wear.preview
 
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
-import androidx.wear.compose.material.Colors
+import com.google.android.horologist.compose.tools.ThemeColors
 import com.google.android.horologist.compose.tools.ThemeValues
-import dev.johnoreilly.confetti.wear.settings.toMaterialThemeColors
+import dev.johnoreilly.confetti.wear.proto.Theme
 
 class ConfettiPreviewThemes : PreviewParameterProvider<ThemeValues> {
     override val values: Sequence<ThemeValues>
         get() = listOf(
-            ThemeValues("Material", 0, Colors()),
-            ThemeValues("MobileDefault", 1, TestFixtures.MobileTheme.toMaterialThemeColors()),
-            ThemeValues("MobileAndroid", 2, TestFixtures.AndroidTheme.toMaterialThemeColors())
+            ThemeValues("Material", 0, ThemeColors()),
+            ThemeValues("MobileDefault", 1, TestFixtures.MobileTheme.toThemeColors()),
+            ThemeValues("MobileAndroid", 2, TestFixtures.AndroidTheme.toThemeColors())
         ).asSequence()
+}
+
+fun Theme.toThemeColors(): ThemeColors {
+    return ThemeColors(
+        primary = Color(primary),
+        primaryVariant = Color(primaryVariant),
+        secondary = Color(secondary),
+        secondaryVariant = Color(secondaryVariant),
+        surface = Color(surface),
+        error = Color(error),
+        onPrimary = Color(onPrimary),
+        onSecondary = Color(onSecondary),
+        onBackground = Color(onBackground),
+        onSurface = Color(onSurface),
+        onSurfaceVariant = Color(onSurfaceVariant),
+        onError = Color(onError),
+    )
 }

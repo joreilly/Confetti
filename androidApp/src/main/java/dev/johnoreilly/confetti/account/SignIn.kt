be5cbf620723e69710c2675c3bdd4e6e2da63bcc
Yuri Schimke <yuri@schimke.ee>
2023-09-17 09:33:10 +0100

    Improve auth flow

--- androidApp/src/main/java/dev/johnoreilly/confetti/account/SignIn.kt
+++ androidApp/src/main/java/dev/johnoreilly/confetti/account/SignIn.kt
@@ -1,22 +1,6 @@
 package dev.johnoreilly.confetti.account
 
 import android.content.Context
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.stringResource
 import androidx.credentials.CredentialManager
 import androidx.credentials.GetCredentialRequest
 import androidx.credentials.exceptions.GetCredentialCancellationException
@@ -26,9 +10,6 @@ import com.google.android.libraries.identity.googleid.GetGoogleIdOption
 import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential
 import dev.johnoreilly.confetti.R
 import dev.johnoreilly.confetti.auth.Authentication
-import dev.johnoreilly.confetti.decompose.SignInComponent
-import kotlinx.coroutines.launch
-import org.koin.compose.koinInject
 
 
 suspend fun signIn(context: Context, authentication: Authentication) {

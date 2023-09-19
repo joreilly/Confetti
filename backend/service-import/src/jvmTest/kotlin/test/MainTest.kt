407f755c49189b6f917951ca40c150fe8a43cbfc
Martin Bonnin <martin@mbonnin.net>
2023-09-19 11:44:41 +0200

    disable test

--- backend/service-import/src/jvmTest/kotlin/test/MainTest.kt
+++ backend/service-import/src/jvmTest/kotlin/test/MainTest.kt
@@ -1,11 +1,5 @@
 package test
 
-import com.google.cloud.datastore.Entity
-import com.google.cloud.datastore.FullEntity
-import com.google.cloud.datastore.StringValue
-import dev.johnoreilly.confetti.backend.datastore.DataStore
-import dev.johnoreilly.confetti.backend.datastore.DataStore.Companion.getListOrNull
-import dev.johnoreilly.confetti.backend.datastore.DataStore.Companion.toValue
 import dev.johnoreilly.confetti.backend.import.SwiftConnection
 import kotlinx.coroutines.runBlocking
 import org.junit.Ignore
@@ -13,6 +7,7 @@ import org.junit.Test
 
 class MainTest {
     @Test
+    @Ignore
     fun test() {
         runBlocking {
             val updated = SwiftConnection.import()

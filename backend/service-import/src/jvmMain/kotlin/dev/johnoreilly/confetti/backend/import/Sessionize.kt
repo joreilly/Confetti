59f92b8a9f239b6ebb9a6dff776d41bf6961bca0
Oleksandr Katrych <me@olek.dev>
2024-02-27 20:06:55 +0100

    Add SHEDEV Warsaw 2024 event

--- backend/service-import/src/jvmMain/kotlin/dev/johnoreilly/confetti/backend/import/Sessionize.kt
+++ backend/service-import/src/jvmMain/kotlin/dev/johnoreilly/confetti/backend/import/Sessionize.kt
@@ -422,6 +422,29 @@ object Sessionize {
         )
     }
 
+    suspend fun importSheDevWarsaw2024(): Int {
+        return writeData(
+            getData("https://sessionize.com/api/v2/b0q6oh1z/view/All"),
+            config = DConfig(
+                id = ConferenceId.SheDevWarsaw2024.id,
+                name = "SHEDEV Warsaw 2024",
+                timeZone = "Europe/Warsaw",
+                ),
+            venue = DVenue(
+                id = "main",
+                name = "Google for Startups Campus Warsaw",
+                address = "Plac Konesera 10, 03-736 Warszawa",
+                description = mapOf(
+                    "en" to "Google for Startups Campus Warsaw",
+                    ),
+                latitude = 52.2561388,
+                longitude = 21.0453105,
+                imageUrl = "https://i.postimg.cc/GmVdqZsq/campus-outside.jpg",
+                floorPlanUrl = null
+            ),
+        )
+    }
+
     internal fun writeData(
         sessionizeData: SessionizeData,
         config: DConfig,

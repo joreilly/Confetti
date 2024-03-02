608d845679d1f6c4e24cc39a351790e81c9d7d21
John O'Reilly <oreillyj@gmail.com>
2024-03-02 14:32:49 +0000

    image loader update + markdown library fix

--- compose-desktop/build.gradle.kts
+++ compose-desktop/build.gradle.kts
@@ -16,7 +16,8 @@ dependencies {
     implementation(compose.material3)
     implementation(compose.components.resources)
 
-    implementation("com.mikepenz:multiplatform-markdown-renderer:0.13.0")
+    implementation("com.mikepenz:multiplatform-markdown-renderer-m3:0.13.0")
+    implementation("com.mikepenz:multiplatform-markdown-renderer-jvm:0.13.0")
     implementation(project(":shared"))
 }
 

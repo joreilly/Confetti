a08f39bb987a0e9d96a51a08fb45465b0c485917
John O'Reilly <oreillyj@gmail.com>
2024-03-09 09:54:51 +0000

    ui updates

--- shared/src/commonMain/kotlin/dev/johnoreilly/confetti/ui/ConferenceListView.kt
+++ shared/src/commonMain/kotlin/dev/johnoreilly/confetti/ui/ConferenceListView.kt
@@ -14,13 +14,17 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MediumTopAppBar
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import conferenceDayMonthFormat
 import dev.johnoreilly.confetti.GetConferencesQuery
 import kotlinx.datetime.LocalDate
@@ -32,25 +36,26 @@ import kotlinx.datetime.LocalDate
 fun ConferenceListView(conferenceListByYear: Map<Int, List<GetConferencesQuery.Conference>>, navigateToConference: (GetConferencesQuery.Conference) -> Unit) {
     Scaffold(
         topBar = {
-            CenterAlignedTopAppBar(
-                title = {
-                    Text(
-                        modifier = Modifier.padding(horizontal = 32.dp),
-                        maxLines = 2,
-                        textAlign = TextAlign.Center,
-                        text = "Confetti",
-                        style = MaterialTheme.typography.titleLarge
-                    )
-                }
-            )
+            Surface(tonalElevation = 0.dp) {
+                MediumTopAppBar(
+                    title = {
+                        Text(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(horizontal = 32.dp),
+                            maxLines = 2,
+                            textAlign = TextAlign.Center,
+                            text = "Confetti",
+                            fontSize = 32.sp,
+                        )
+                    }
+                )
+            }
         }) {
         LazyColumn(Modifier.padding(it)) {
             conferenceListByYear.forEach { (year, conferenceList) ->
                 stickyHeader {
-                    Text(text = year.toString(),
-                        modifier = Modifier.fillMaxWidth(),
-                        textAlign = TextAlign.Center,
-                        style = MaterialTheme.typography.titleLarge)
+                    YearHeader(year.toString())
                 }
 
                 items(conferenceList) { conference ->
@@ -63,19 +68,24 @@ fun ConferenceListView(conferenceListByYear: Map<Int, List<GetConferencesQuery.C
 
 
 @Composable
-fun ConferenceCard(conference: GetConferencesQuery.Conference, navigateToConference: (GetConferencesQuery.Conference) -> Unit) {
+fun ConferenceCard(
+    conference: GetConferencesQuery.Conference,
+    navigateToConference: (GetConferencesQuery.Conference) -> Unit
+) {
     ConferenceMaterialTheme(conference.themeColor) {
-        Card(modifier = Modifier
-            .clip(shape = RoundedCornerShape(8.dp))
-            .clickable(onClick = {
-                navigateToConference(conference)
-            })
-            .padding(8.dp)
-            .fillMaxWidth()
+        Card(
+            modifier = Modifier
+                .clip(shape = RoundedCornerShape(8.dp))
+                .clickable(onClick = {
+                    navigateToConference(conference)
+                })
+                .padding(8.dp)
+                .fillMaxWidth()
         ) {
-            Column(Modifier.padding(16.dp)) {
+            Column(Modifier.fillMaxWidth().padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally) {
                 Text(conference.name, style = MaterialTheme.typography.titleMedium)
-                Text(getConferenceDatesString(conference.days), style = MaterialTheme.typography.titleSmall)
+                Text(getConferenceDatesString(conference.days), style = MaterialTheme.typography.bodyLarge)
             }
         }
     }
@@ -91,4 +101,21 @@ private fun getConferenceDatesString(days: List<LocalDate>): String {
         conferenceDatesString += " - ${days[1].conferenceDayMonthFormat()}"
     }
     return conferenceDatesString
+}
+
+@Composable
+fun YearHeader(
+    text: String,
+    modifier: Modifier = Modifier,
+) {
+    Surface(
+        color = MaterialTheme.colorScheme.surface,
+        modifier = modifier.fillMaxWidth(),
+    ) {
+        Text(
+            text = text,
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.titleLarge
+        )
+    }
 }

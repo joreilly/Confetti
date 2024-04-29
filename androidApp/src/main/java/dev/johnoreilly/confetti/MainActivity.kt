991b79626fedae834c92e2023b24f38e5a1d59bf
Arkadii Ivanov <arkann1985@gmail.com>
2024-04-29 22:54:27 +0100

    Updated Decompose to 3.0.0 and Essenty to 2.0.0, used the new handleDeepLink API

--- androidApp/src/main/java/dev/johnoreilly/confetti/MainActivity.kt
+++ androidApp/src/main/java/dev/johnoreilly/confetti/MainActivity.kt
@@ -2,8 +2,7 @@
 
 package dev.johnoreilly.confetti
 
-import android.app.TaskStackBuilder
-import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -21,6 +20,7 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import com.arkivanov.decompose.defaultComponentContext
+import com.arkivanov.decompose.handleDeepLink
 import dev.johnoreilly.confetti.account.signIn
 import dev.johnoreilly.confetti.auth.Authentication
 import dev.johnoreilly.confetti.decompose.DefaultAppComponent
@@ -31,10 +31,7 @@ import dev.johnoreilly.confetti.ui.component.ConfettiBackground
 import kotlinx.coroutines.launch
 import org.koin.android.ext.android.inject
 
-
 class MainActivity : ComponentActivity() {
-
-    private var isDeepLinkHandledPreviously = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -58,29 +55,26 @@ class MainActivity : ComponentActivity() {
         // including IME animations
         WindowCompat.setDecorFitsSystemWindows(window, false)
 
-        isDeepLinkHandledPreviously = savedInstanceState?.getBoolean(KEY_DEEP_LINK_HANDLED) ?: false
-        val initialConferenceId = extractConferenceIdOrNull(intent, isDeepLinkHandledPreviously)
-        if (initialConferenceId != null) {
-            intent.setData(null)
-            isDeepLinkHandledPreviously = true
-        }
         val appComponent =
-            DefaultAppComponent(
-                componentContext = defaultComponentContext(
-                    discardSavedState = initialConferenceId != null,
-                ),
-                initialConferenceId = initialConferenceId,
-                onSignOut = {
-                    lifecycleScope.launch {
-                        credentialManager.clearCredentialState(ClearCredentialStateRequest())
+            handleDeepLink { uri ->
+                val initialConferenceId = uri?.extractConferenceIdOrNull()
+                DefaultAppComponent(
+                    componentContext = defaultComponentContext(
+                        discardSavedState = initialConferenceId != null,
+                    ),
+                    initialConferenceId = initialConferenceId,
+                    onSignOut = {
+                        lifecycleScope.launch {
+                            credentialManager.clearCredentialState(ClearCredentialStateRequest())
+                        }
+                    },
+                    onSignIn = {
+                        lifecycleScope.launch {
+                            signIn(this@MainActivity, authentication)
+                        }
                     }
-                },
-                onSignIn = {
-                    lifecycleScope.launch {
-                        signIn(this@MainActivity, authentication)
-                    }
-                }
-            )
+                )
+            } ?: return
 
         setContent {
             val windowSizeClass = calculateWindowSizeClass(this)
@@ -101,52 +95,18 @@ class MainActivity : ComponentActivity() {
     }
 
     /**
-     * From a deep link like `https://confetti-app.dev/conference/devfeststockholm2023` extracts `devfeststockholm2023`
-     * In case we were asked to create a new task with [Intent.FLAG_ACTIVITY_NEW_TASK] it clears the entire activity
-     * and starts a new one to be in a predictably good state
+     * From a deep link like `https://confetti-app.dev/conference/devfeststockholm2023` extracts `devfeststockholm2023`.
      */
-    private fun extractConferenceIdOrNull(intent: Intent, isDeepLinkHandledPreviously: Boolean): String? {
-        val flags = intent.flags
-        if (flags and Intent.FLAG_ACTIVITY_NEW_TASK != 0 && flags and Intent.FLAG_ACTIVITY_CLEAR_TASK == 0) {
-            // From:
-            // https://cs.android.com/androidx/platform/frameworks/support/+/androidx-main:navigation/navigation-runtime/src/main/java/androidx/navigation/NavController.kt;l=1487-1504?q=Intent.flags&ss=androidx%2Fplatform%2Fframeworks%2Fsupport:navigation%2F
-            // Someone called us with NEW_TASK, but we don't know what state our whole
-            // task stack is in, so we need to manually restart the whole stack to
-            // ensure we're in a predictably good state.
-            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
-            val taskStackBuilder = TaskStackBuilder
-                .create(this@MainActivity)
-                .addNextIntentWithParentStack(intent)
-            taskStackBuilder.startActivities()
-            this.finish()
-            // Disable second animation in case where the Activity is created twice.
-            @Suppress("DEPRECATION")
-            this.overridePendingTransition(0, 0)
-            return null
-        }
-        if (isDeepLinkHandledPreviously) {
-            return null
-        }
-        with(intent.data ?: return null) {
-            if (host != "confetti-app.dev") return null
-            val path = path ?: return null
-            if (path.firstOrNull() != '/') return null
-            val parts = path.substring(1).split('/')
-            if (parts.size != 2) return null
-            if (parts[0] != "conference") return null
-            val conferenceId = parts[1]
-            if (!conferenceId.all { it.isLetterOrDigit() }) return null
-            return conferenceId
-        }
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putBoolean(KEY_DEEP_LINK_HANDLED, isDeepLinkHandledPreviously)
-    }
-
-    companion object {
-        const val KEY_DEEP_LINK_HANDLED: String = "dev.johnoreilly.confetti.KEY_DEEP_LINK_HANDLED"
+    private fun Uri.extractConferenceIdOrNull(): String? {
+        if (host != "confetti-app.dev") return null
+        val path = path ?: return null
+        if (path.firstOrNull() != '/') return null
+        val parts = path.substring(1).split('/')
+        if (parts.size != 2) return null
+        if (parts[0] != "conference") return null
+        val conferenceId = parts[1]
+        if (!conferenceId.all { it.isLetterOrDigit() }) return null
+        return conferenceId
     }
 }
 

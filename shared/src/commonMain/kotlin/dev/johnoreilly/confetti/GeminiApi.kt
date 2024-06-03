99f55e2e6977f5d15b3076cc9c7cdf8531c0fa77
John O'Reilly <oreillyj@gmail.com>
2024-06-03 19:23:06 +0100

    gemini based recommendations updates

--- shared/src/commonMain/kotlin/dev/johnoreilly/confetti/GeminiApi.kt
+++ shared/src/commonMain/kotlin/dev/johnoreilly/confetti/GeminiApi.kt
@@ -2,36 +2,57 @@ package dev.johnoreilly.confetti
 
 import dev.shreyaspatil.ai.client.generativeai.GenerativeModel
 import dev.shreyaspatil.ai.client.generativeai.type.BlockThreshold
+import dev.shreyaspatil.ai.client.generativeai.type.Content
 import dev.shreyaspatil.ai.client.generativeai.type.GenerateContentResponse
+import dev.shreyaspatil.ai.client.generativeai.type.GenerationConfig
 import dev.shreyaspatil.ai.client.generativeai.type.HarmCategory
 import dev.shreyaspatil.ai.client.generativeai.type.PlatformImage
 import dev.shreyaspatil.ai.client.generativeai.type.SafetySetting
 import dev.shreyaspatil.ai.client.generativeai.type.content
 import kotlinx.coroutines.flow.Flow
-import kotlin.io.encoding.ExperimentalEncodingApi
+
 
 class GeminiApi {
     private val apiKey =  BuildKonfig.GEMINI_API_KEY
 
-    val generativeVisionModel = GenerativeModel(
-        modelName = "gemini-pro-vision",
-        apiKey = apiKey
-    )
+    fun generateContentStream(prompt: String): Flow<GenerateContentResponse> {
+        val configBuilder = GenerationConfig.Builder()
+        configBuilder.temperature = 0.0f
 
-    val generativeModel = GenerativeModel(
-        //modelName = "gemini-pro",
-        modelName = "gemini-1.5-flash",
-        apiKey = apiKey,
-        safetySettings = listOf(SafetySetting(HarmCategory.DANGEROUS_CONTENT, BlockThreshold.ONLY_HIGH))
+        val generativeModel = GenerativeModel(
+            modelName = "gemini-1.5-flash",
+            apiKey = apiKey,
+            generationConfig = configBuilder.build(),
+            safetySettings = listOf(SafetySetting(HarmCategory.DANGEROUS_CONTENT, BlockThreshold.ONLY_HIGH))
+        )
 
-    )
-
-    fun generateContent(prompt: String): Flow<GenerateContentResponse> {
         return generativeModel.generateContentStream(prompt)
     }
 
-    @OptIn(ExperimentalEncodingApi::class)
+    suspend fun generateContent(prompt: String, query: String): GenerateContentResponse {
+        val configBuilder = GenerationConfig.Builder()
+        configBuilder.temperature = 0.0f
+
+        val contentBuilder = Content.Builder()
+        contentBuilder.text(prompt)
+
+        val generativeModel = GenerativeModel(
+            modelName = "gemini-1.5-flash",
+            apiKey = apiKey,
+            systemInstruction = contentBuilder.build(),
+            generationConfig = configBuilder.build(),
+            safetySettings = listOf(SafetySetting(HarmCategory.DANGEROUS_CONTENT, BlockThreshold.ONLY_HIGH))
+        )
+
+        return generativeModel.generateContent(query)
+    }
+
     fun generateContent(prompt: String, imageData: ByteArray): Flow<GenerateContentResponse> {
+        val generativeVisionModel = GenerativeModel(
+            modelName = "gemini-pro-vision",
+            apiKey = apiKey
+        )
+
         val content = content {
             image(PlatformImage(imageData))
             text(prompt)

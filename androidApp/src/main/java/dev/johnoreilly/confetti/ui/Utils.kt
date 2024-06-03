5a2858f7824ff67afb089b8eb05e5b07f2c19037
Marcello Galhardo <marcello.galhardo@gmail.com>
2024-06-03 22:01:30 +0100

    Collapsing Toolbar in Home Scaffold

--- androidApp/src/main/java/dev/johnoreilly/confetti/ui/Utils.kt
+++ androidApp/src/main/java/dev/johnoreilly/confetti/ui/Utils.kt
@@ -5,3 +5,6 @@ import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 
 val WindowSizeClass.isExpanded: Boolean
     get() = widthSizeClass == WindowWidthSizeClass.Expanded
+
+val WindowSizeClass.isCompact: Boolean
+    get() = widthSizeClass == WindowWidthSizeClass.Compact

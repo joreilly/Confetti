bb5722e406f7181d3236b45d681538f3bb34fb7d
Yuri Schimke <yuri@schimke.ee>
2025-07-18 19:05:33 +0100

    Fix compile issue

--- wearApp/src/main/java/dev/johnoreilly/confetti/wear/tile/TileSync.kt
+++ wearApp/src/main/java/dev/johnoreilly/confetti/wear/tile/TileSync.kt
@@ -23,18 +23,6 @@ class TileSync(
         registry.dataClient
             .putDataItem(PutDataRequest.create("/tile_tracking_enabled")).await()
 
-        val myTilesList = listOf(
-            CurrentSessionsTileService::class.java.name,
-        )
-
-        val activeTiles = TileService.getActiveTilesAsync(context, executor).await()
-
-        for (tileName in myTilesList) {
-            if (activeTiles.any { it.componentName.className == tileName }) {
-                wearAppHelper.markTileAsInstalled(tileName)
-            } else {
-                wearAppHelper.markTileAsRemoved(tileName)
-            }
-        }
+        wearAppHelper.updateInstalledTiles()
     }
 }

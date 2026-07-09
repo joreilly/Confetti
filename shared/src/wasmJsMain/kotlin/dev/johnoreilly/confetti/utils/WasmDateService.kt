4b214572982f3cd514d35f63f195c834f50723d2
Ilya Goncharov <ilgonmic@gmail.com>
2026-07-09 20:39:32 +0100

    Fix compilation by replacing deprecated kotlinx.datetime.Clock with kotlin.time.Clock

--- shared/src/wasmJsMain/kotlin/dev/johnoreilly/confetti/utils/WasmDateService.kt
+++ shared/src/wasmJsMain/kotlin/dev/johnoreilly/confetti/utils/WasmDateService.kt
@@ -1,9 +1,9 @@
 package dev.johnoreilly.confetti.utils
 
-import kotlinx.datetime.Clock
 import kotlinx.datetime.LocalDateTime
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toLocalDateTime
+import kotlin.time.Clock
 
 class WasmDateService: DateService {
     override fun now(): LocalDateTime = Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault())

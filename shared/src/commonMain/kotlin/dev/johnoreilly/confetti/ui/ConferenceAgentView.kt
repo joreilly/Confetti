a5b8bd78fb91d488d527b6b7c179bec54b064c86
Marcello Galhardo <mgalhardo@google.com>
2026-08-11 22:31:24 +0200

    Add IME padding to ConferenceAgentView
    Prevent keyboard from covering text input field.
    
    Test: Compile only.

--- shared/src/commonMain/kotlin/dev/johnoreilly/confetti/ui/ConferenceAgentView.kt
+++ shared/src/commonMain/kotlin/dev/johnoreilly/confetti/ui/ConferenceAgentView.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -60,6 +61,7 @@ fun ConferenceAgentView(component: ConferenceAgentComponent, bottomContentPaddin
     Column(
         modifier = Modifier
             .fillMaxSize()
+            .imePadding()
             .padding(16.dp)
             .padding(bottom = bottomContentPadding)
     ) {

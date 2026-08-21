5138bb07c74ec932e155d825ac55fbc7cbba0341
Martin Bonnin <martin@mbonnin.net>
2026-08-21 15:58:37 +0200

    Revert "Merge pull request #1867 from joreilly/query"
    This reverts commit 3c62862b95b817dd8cc8eefe766c9559ff576ab0, reversing
    changes made to 766a9e437d75563815c99c306b5b2ee830f07195.

--- backend/service-graphql/src/main/kotlin/dev/johnoreilly/confetti/backend/DefaultApplication.kt
+++ backend/service-graphql/src/main/kotlin/dev/johnoreilly/confetti/backend/DefaultApplication.kt
@@ -33,7 +33,6 @@ import org.springframework.context.ApplicationContext
 import org.springframework.context.ApplicationListener
 import org.springframework.context.ConfigurableApplicationContext
 import org.springframework.context.annotation.Bean
-import org.springframework.http.HttpMethod
 import org.springframework.http.MediaType
 import org.springframework.http.client.reactive.JdkClientHttpConnector
 import org.springframework.http.codec.ServerCodecConfigurer
@@ -68,7 +67,7 @@ class DefaultApplication {
         errorAttributes: ErrorAttributes?,
         webProperties: WebProperties,
         serverProperties: ServerProperties,
-        viewResolvers: ObjectProvider<ViewResolver>,
+        viewResolvers: ObjectProvider<ViewResolver?>,
         serverCodecConfigurer: ServerCodecConfigurer,
         applicationContext: ApplicationContext?
     ): ErrorWebExceptionHandler? {
@@ -133,7 +132,7 @@ class DefaultApplication {
             )
         }
 
-        (POST("/graphql") or GET("/graphql") or RequestPredicates.method(HttpMethod.QUERY)).invoke { serverRequest ->
+        (POST("/graphql") or GET("/graphql")).invoke { serverRequest ->
             var conference = serverRequest.queryParam("conference").getOrNull()
             if (conference == null) {
                 conference = serverRequest.headers().firstHeader("conference")
